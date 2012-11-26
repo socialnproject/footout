4b96ee9605682fe2daa9cdfae617c99b73a4a69e
OpenShift guest <as1511488@gmail.com>
2012-11-25 19:41:56 -0500

    ola

--- public/js/views/matchlist.js
+++ public/js/views/matchlist.js
@@ -10,14 +10,18 @@ window.MatchListView = Backbone.View.extend({
         var matchesperpage = 100;
         var startPos = (this.options.page - 1) * matchesperpage;
         var endPos = Math.min(startPos + matchesperpage, len);
+        
 
         $(this.el).html('<ul class="thumbnails"></ul>');
+
+        $('.thumbnails', this.el).append('<a href="#" ><div class="zocial">Data</div></a>');
+        $('.thumbnails', this.el).append('<a href="#" ><div class="zocial">Ocupação</div></a>');
 
         for (var i = startPos; i < endPos; i++) {
             $('.thumbnails', this.el).append(new MatchListItemView({model: matches[i]}).render().el);
         }
 
-        $(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
+        //$(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
 
         return this;
     }
